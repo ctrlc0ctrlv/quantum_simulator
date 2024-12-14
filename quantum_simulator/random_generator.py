b1e087404a020368a1b3957a5782093a35272889
ctrlc0ctrlv <46068723+ctrlc0ctrlv@users.noreply.github.com>
2024-12-15 00:29:07 +0300

    QS-9_refactor_random_generator (#10)
    * Refactored class properties
    
    * Added function for random unitary operator
    
    * Updated tests

--- quantum_simulator/random_generator.py
+++ quantum_simulator/random_generator.py
@@ -1,46 +1,95 @@
+"""Random generator module"""
+
+import numpy as np
+import scipy as sc
+
+
 class RandomGenerator:
     """
     Random generator class
     """
 
-    m: int
+    _m: int
     "m value"
 
-    a: int
+    _a: int
     "a value"
 
-    c: int
+    _c: int
     "c value"
 
-    seed: int
+    _seed: int
     "generator seed"
 
-    X: int
+    _X: int
     "current generator value"
 
-    def __init__(self, m=65537, a=75, c=74, seed=27):
+    def __init__(self, m: int = 65537, a: int = 75, c: int = 74, seed: int = 27):
         """
         RandomGenerator initializer
         """
-        self.m = m
-        self.a = a
-        self.c = c
-        self.seed = seed
-        self.X = self.generate_initial_num(seed, n0=99)
+        self._m = m
+        self._a = a
+        self._c = c
+        self._seed = seed
+        self._state = self.generate_initial_num(seed, n0=99)
 
     def rand_int(self):
         """Returns random integer value. Updates self"""
-        self.X = (self.a * self.X + self.c) % self.m
-        return self.X
+        self._state = (self.a * self._state + self.c) % self.m
+        return self._state
 
-    def rand(self, L=None):
+    def rand(self, L: int = None):
         """Returns random number. Updates self"""
-        self.X = self.rand_int()
-        return self.X / self.m if L is None else (self.X / self.m - 0.5) * 2 * L
+        self._state = self.rand_int()
+        return self._state / self.m if L is None else (self._state / self.m - 0.5) * 2 * L
 
-    def generate_initial_num(self, seed, n0):
+    def generate_initial_num(self, seed: int, n0: int):
         """Skips `n0` random numbers"""
-        self.X = seed
+        self._state = seed
         for _ in range(n0):
-            self.X = self.rand_int()
-        return self.X
+            self._state = self.rand_int()
+        return self._state
+
+    @property
+    def m(self):
+        """RandomGenerator._m readonly getter"""
+        return self._m
+
+    @property
+    def a(self):
+        """RandomGenerator._a readonly getter"""
+        return self._a
+
+    @property
+    def c(self):
+        """RandomGenerator._c readonly getter"""
+        return self._c
+
+    @property
+    def seed(self):
+        """RandomGenerator._seed readonly getter"""
+        return self._seed
+
+    @property
+    def state(self):
+        """RandomGenerator._state readonly getter"""
+        return self._state
+
+    def rand_unitary(self, mode: str) -> np.ndarray:
+        """Builds and returns random unitary matrix using random generator"""
+        assert mode in ["1q", "2q"], f"'{mode}' mode is not supported, should be '1q' or '2q'"
+        if mode == "1q":
+            c = [self.rand(L=100) for _ in range(4)]
+            U = 1j * np.array([[c[0], c[1] + 1j * c[2]], [c[1] - 1j * c[2], c[3]]])
+        else:
+            c = [self.rand(L=100) for _ in range(16)]
+            U = 1j * np.array(
+                [
+                    [c[0], c[1] + 1j * c[2], c[3] + 1j * c[4], c[5] + 1j * c[6]],
+                    [c[1] - 1j * c[2], c[7], c[8] + 1j * c[9], c[10] + 1j * c[11]],
+                    [c[3] - 1j * c[4], c[8] - 1j * c[9], c[12], c[13] + 1j * c[14]],
+                    [c[5] - 1j * c[6], c[10] - 1j * c[11], c[13] - 1j * c[14], c[15]],
+                ]
+            )
+        return sc.linalg.expm(U)
--- quantum_simulator/test/test_random_generator.py
+++ quantum_simulator/test/test_random_generator.py
@@ -2,23 +2,36 @@
 
 from unittest import TestCase
 
+import numpy as np
 import pytest
 
 from quantum_simulator.random_generator import RandomGenerator
 
 
-@pytest.mark.quant_oper
-class TestQuantumOperation(TestCase):
+@pytest.mark.random
+class TestRandomGenerator(TestCase):
     """RandomGenerator tests class"""
 
     def test_random_generator_init(self):
         """Tests RandomGenerator init"""
         rand_gen = RandomGenerator()
-        self.assertEqual(rand_gen.X, 14715)
+        self.assertEqual(rand_gen.state, 14715)
         self.assertEqual(rand_gen.m, 65537)
         self.assertEqual(rand_gen.a, 75)
         self.assertEqual(rand_gen.c, 74)
         self.assertEqual(rand_gen.seed, 27)
+
+        # check that generator properties are read-only
+        with self.assertRaises(AttributeError):
+            rand_gen.state = 0
+        with self.assertRaises(AttributeError):
+            rand_gen.m = 0
+        with self.assertRaises(AttributeError):
+            rand_gen.a = 0
+        with self.assertRaises(AttributeError):
+            rand_gen.c = 0
+        with self.assertRaises(AttributeError):
+            rand_gen.seed = 0
 
     def test_random_generator_rand(self):
         """Tests RandomGenerator rand()"""
@@ -39,3 +52,21 @@ class TestQuantumOperation(TestCase):
         self.assertEqual(rand_int, 4268)
         rand_int = rand_gen.rand_int()
         self.assertEqual(rand_int, 58026)
+
+    def test_get_rand_unitary(self):
+        """Tests RandomGenerator rand_unitary()"""
+        rand_gen = RandomGenerator()
+        U1 = rand_gen.rand_unitary("1q")
+        target_U1 = np.array([[-0.15993173 - 0.31217771j, 0.87691877 + 0.32860369j], [-0.22080244 + 0.9100622j, 0.16840845 + 0.30768763j]])
+        self.assertTrue(np.isclose(U1, target_U1).all())
+
+        U2 = rand_gen.rand_unitary("2q")
+        target_U2 = np.array(
+            [
+                [-0.3719458 - 0.01701984j, 0.1913409 - 0.176701j, 0.72057625 + 0.39256476j, -0.33574361 - 0.08643538j],
+                [0.60583561 + 0.35087279j, 0.45208891 + 0.36159583j, 0.09476359 - 0.00109205j, -0.38687635 - 0.12673169j],
+                [-0.05039977 + 0.22385688j, 0.57039366 - 0.04917816j, 0.16956984 + 0.01920575j, 0.76592686 + 0.06175591j],
+                [0.49953428 - 0.26273866j, -0.46268193 + 0.2349202j, 0.51836348 + 0.14091826j, 0.35152864 - 0.00638688j],
+            ]
+        )
+        self.assertTrue(np.isclose(U2, target_U2).all())
