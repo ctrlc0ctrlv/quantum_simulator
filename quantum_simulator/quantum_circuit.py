6fa561251e7a5396d19803730d25190f9dfa1b3a
ctrlc0ctrlv <vrbyov12@gmail.com>
2024-12-15 01:33:00 +0300

    Added new constructor argument; refactored code

--- quantum_simulator/quantum_circuit.py
+++ quantum_simulator/quantum_circuit.py
@@ -1,16 +1,20 @@
 """Quantum circuit module"""
 
+from quantum_simulator.quantum_operation import OneQubitOperation, TwoQubitsOperation
 from quantum_simulator.random_generator import RandomGenerator
 
 
 class QuantumCircuit:
     """Quantum circuit class"""
 
-    num_qubits: int
+    _width: int
     "Number of qubits in circuit"
 
-    depth: int
+    _depth: int
     "Max number of gates"
+
+    weight_2q: float
+    "2-qubits operation weight"
 
     seed: int
     "Random seed"
@@ -18,8 +22,23 @@ class QuantumCircuit:
     random_generator: RandomGenerator = None
     "Random unitary gates generator"
 
-    def __init__(self, num_qubits: int, depth: int, seed: int = 27):
-        self.num_qubits = num_qubits
-        self.depth = depth
+    gate_layers: list = None
+    "Circuit gate layers - `[(list_of_layer_gates, set_of_targeted_qubits)]`"
+
+    def __init__(self, width: int, depth: int, weight_2q: float, seed: int = 27):
+        self._width = width
+        self._depth = depth
+        self.weight_2q = weight_2q
         self.seed = seed
         self.random_generator = RandomGenerator(seed=self.seed)
+        self.gate_layers = []
+
+    @property
+    def depth(self):
+        """Max number of gates read-only property"""
+        return self._depth
+
+    @property
+    def width(self):
+        """Number of qubits in circuit read-only property"""
+        return self._width
