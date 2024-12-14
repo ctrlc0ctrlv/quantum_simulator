e03430946c58088151d41d95d942ba857c0533a5
ctrlc0ctrlv <vrbyov12@gmail.com>
2024-12-15 01:33:46 +0300

    Added new method

--- quantum_simulator/quantum_circuit.py
+++ quantum_simulator/quantum_circuit.py
@@ -42,3 +42,36 @@ class QuantumCircuit:
     def width(self):
         """Number of qubits in circuit read-only property"""
         return self._width
+    
+    def generate_gates_and_unite(self) -> None:
+        """
+        Generates random gates.
+
+        Also compresses the layer, so that each layer has as many simultaneous operations as possible
+        """
+        for _ in range(self.depth):
+            rand_num = self.random_generator.rand()
+            if rand_num >= self.weight_2q or self.width == 1:
+                q = self.random_generator.rand_int() % self.width
+                U = self.random_generator.rand_unitary(mode="1q")
+                gate = OneQubitOperation(U, [q])
+                q1, q2 = q, q
+            else:
+                q1 = self.random_generator.rand_int() % self.width
+                q2 = self.random_generator.rand_int() % self.width
+                while q2 == q1:
+                    q2 = self.random_generator.rand_int() % self.width
+                U = self.random_generator.rand_unitary(mode="2q")
+                gate = TwoQubitsOperation(U, [q1, q2])
+
+            # compress gate layers if possible
+            idx = len(self.gate_layers) - 1
+            while idx >= 0 and q1 not in self.gate_layers[idx][1] and q2 not in self.gate_layers[idx][1]:
+                idx -= 1
+            idx += 1
+            if idx == len(self.gate_layers):
+                self.gate_layers.append(([gate], {q1, q2}))
+            else:
+                self.gate_layers[idx][0].append(gate)
+                self.gate_layers[idx][1].add(q1)
+                self.gate_layers[idx][1].add(q2)
