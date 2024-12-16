24f18e894aff43ec33fd8a4dded05a2694d46929
ctrlc0ctrlv <vrbyov12@gmail.com>
2024-12-16 22:20:57 +0300

    Added abstract quantum emulator module

---
+++ quantum_simulator/abstract_quantum_emulator.py
@@ -0,0 +1,19 @@
+"""Abstract quantum emulator module"""
+
+from abc import ABC, abstractmethod
+
+from quantum_simulator.quantum_circuit import QuantumCircuit
+from quantum_simulator.quantum_operation import QuantumOperation
+from quantum_simulator.quantum_state_vector import QuantumStateVector
+
+
+class AbstractQuantumEmulator(ABC):
+    """Abstract quantum emulator class"""
+
+    @abstractmethod
+    def apply_gate(self, operation: QuantumOperation, state_vector: QuantumStateVector) -> QuantumStateVector:
+        """Applies quantum operation to a given state vector"""
+
+    @abstractmethod
+    def apply_circuit(self, circuit: QuantumCircuit, state_vector: QuantumStateVector) -> QuantumStateVector:
+        """Applies quantum circuit to a given state vector"""
